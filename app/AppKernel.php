aaabd0441af399f2a7754023bd11eab5bdbd9a32
Sufiyan Malek <sufiyanmalek78@gmail.com>
2016-07-03 18:56:31 +0530

    [settings]

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new CodeExplorerBundle\CodeExplorerBundle(),
             new AppBundle\AppBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         // Some bundles are only used while developing the application or during
